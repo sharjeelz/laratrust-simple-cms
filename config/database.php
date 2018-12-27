326668775e1bab40098ee703ddb0112bc2e1181c
Sharjeel Zubair <szubair.ka@emaar.ae>
2018-12-27 17:24:02 +0400

    added configuration for EBS AWS

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $_SERVER['RDS_HOSTNAME'],
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' =>$_SERVER['RDS_DB_NAME'],
+            'username' =>$_SERVER['RDS_USERNAME'],
+            'password' => $_SERVER['RDS_PASSWORD'],
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
